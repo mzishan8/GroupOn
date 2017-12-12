d930d8092011ac06d9f43e0a1c14101ae0fff52d
mzishan8 <32188725+mzishan8@users.noreply.github.com>
2017-12-12 11:17:07 +0530

    old css and list view and user profile

--- src/ChatClient/ChatRoom/MainChatController.java
+++ src/ChatClient/ChatRoom/MainChatController.java
@@ -7,17 +7,28 @@ package ChatClient.ChatRoom;
 
 import ChatClient.Client;
 import ChatClient.Main;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Separator;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
 
 /**
  *
@@ -29,30 +40,96 @@ public class MainChatController implements Initializable{
     @FXML
     Pane chatPane;
     @FXML
+    Label userName;
+    @FXML
     Pane fPPane;
     @FXML
     Separator chatPSeprator;
     @FXML
     ListView<String> userList;
+    @FXML
+    ListView<Label> msgList;
+    @FXML
+    Label chatUser;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         chatPane.setVisible(false);
         fPPane.setVisible(false);
         chatPSeprator.setVisible(false);
+       // Platform.runLater(() -> {
+            userName.setText(Main.cl.getUserName());
+        //});
+        
        // ReadMsg read = new ReadMsg(userList);
         //userList.
-        new Thread (new ReadMsg(userList)).start();
+//        Platform.runLater(new Runnable() {
+//            @Override
+//            public void run() {
+//                userList.setOnMouseClicked(e ->{
+//            listClicked();
+//        });//To change body of generated methods, choose Tools | Templates.
+//            }
+//        });
+        
+        new Thread (new ReadMsg(userList,msgList)).start();
     }
-
+    @FXML
+    TextField message;
+    @FXML
+    private void sendAction(ActionEvent event) throws IOException{
+        if(!message.equals(null)){
+           Main.cl.write(message.getText());
+            Platform.runLater(new Runnable() {
+               @Override
+               public void run() {
+                   msgList.getItems().add(new Label(message.getText()));
+               }
+           });
+        }     
+    }
+    @FXML
+    private void listClicked(MouseEvent e){
+        chatPane.setVisible(true);
+        fPPane.setVisible(true);
+        String user =  userList.getSelectionModel().getSelectedItem();
+        //System.out.println(user);
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                chatUser.setText(user);//To change body of generated methods, choose Tools | Templates.
+            }
+        });    
+    }
+    @FXML
+    ImageView myPic;
+    @FXML
+    private void profilePicSet(MouseEvent e) throws MalformedURLException{
+        FileChooser chooser = new FileChooser();
+        FileChooser.ExtensionFilter extFilejpg = new FileChooser.ExtensionFilter("jpg files (*.jpg)", "*.jpg" );
+        FileChooser.ExtensionFilter extFileJPG = new FileChooser.ExtensionFilter("JPG files (*.JPG)", "*.JPG" );
+        FileChooser.ExtensionFilter extFilepng = new FileChooser.ExtensionFilter("png files (*.png)", "*.png" );
+        FileChooser.ExtensionFilter extFilePNG = new FileChooser.ExtensionFilter("PNG files (*.PNG)", "*.PNG" );
+        chooser.getExtensionFilters().addAll(extFilejpg,extFileJPG,extFilepng,extFilePNG);
+        File file =chooser.showOpenDialog(null);
+        //Image img = new Image(file.getAbsolutePath());
+        URL url = file.toURI().toURL();
+        myPic.setImage(new Image(url.toExternalForm()));
+//        Platform.runLater(() -> {
+//            myPic.setImage(img);
+//        });
+    }
+    
 }
 class ReadMsg extends Task<Integer>{
     
        ObservableList observableList = FXCollections.observableArrayList();
+       ObservableList<Label> observableMsgList = FXCollections.observableArrayList();
         Client client = Main.cl;     
         ListView<String> userList;
-
-    public ReadMsg(ListView<String> userList) {
+        ListView<Label> msgList;
+    public ReadMsg(ListView<String> userList, ListView<Label> msgList) {
         this.userList= userList;
+        this.msgList = msgList;
     }
         
     @Override
@@ -74,9 +151,16 @@ class ReadMsg extends Task<Integer>{
                          userList.getItems().clear();
                          userList.getItems().addAll(observableList);//To change body of generated methods, choose Tools | Templates.
                      }
-                 });
-                
+                 });     
+             }
+             else if(msg.startsWith("ChatServer: ")){
                  
+             }
+             else{
+                 Label msgLabel = new Label(msg);
+                 Platform.runLater(() -> {
+                      msgList.getItems().add(msgLabel);
+                 });
              }
         }    
     }
